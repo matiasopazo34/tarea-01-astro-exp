6477edfd0d647569d5b9922862575453d56de4b7
Matias <50592733+matiasopazo34@users.noreply.github.com>
2024-04-16 15:14:46 -0400

    tarea 01

--- tarea 01/#### Tarea 01 Astronomia Experimental.py
+++ tarea 01/#### Tarea 01 Astronomia Experimental.py
@@ -1,1 +1,7 @@
 #### Tarea 01 Astronomia Experimental
+import numpy as np
+import panda as pd
+import matplotlib.pyplot as plt
+
+# parte 2
+path = "C:\Users\matia\Desktop\Astro Exp\tarea-01-astro-exp\tarea 01\HCT_AE2023A.xlsx"
